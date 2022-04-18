e2ff1680d9de913c598717a9a4ca36e70c301aba
Luca Scaringella <32013416+LucaCode@users.noreply.github.com>
2022-04-18 11:08:43 +0200

    Improve machine-id generation.

--- src/machineId.ts
+++ src/machineId.ts
@@ -1,18 +1,25 @@
+/*
+Author: Ing. Luca Gian Scaringella
+GitHub: LucaCode
+Copyright(c) Ing. Luca Gian Scaringella
+ */
+
 import * as os from 'os';
 
-let mac = '';
-export let machineId = '';
-const networkInterfaces = os.networkInterfaces();
-if(networkInterfaces != null){
-    loop: for(const interfaceKey in networkInterfaces){
-            const networkInterface = networkInterfaces[interfaceKey];
-            if(!networkInterface) continue;
-            const length = networkInterface.length;
-            for(let i = 0; i < length; i++){
-                if(networkInterface[i] !== undefined && networkInterface[i].mac && networkInterface[i].mac != '00:00:00:00:00:00'){
-                    mac = networkInterface[i].mac; break loop;
-                }
+function getFirstMacAddress() {
+    const networkInterfaces = os.networkInterfaces();
+    if(networkInterfaces == null) return "";
+    for(const interfaceKey in networkInterfaces){
+        const networkInterface = networkInterfaces[interfaceKey];
+        if(!networkInterface) continue;
+        const length = networkInterface.length;
+        for(let i = 0; i < length; i++){
+            if(networkInterface[i] !== undefined && networkInterface[i].mac && networkInterface[i].mac != '00:00:00:00:00:00'){
+                return networkInterface[i].mac;
             }
         }
-    machineId = mac ? parseInt(mac.replace(/\:|\D+/gi, '')).toString(36) : '' ;
+    }
 }
+
+const macAddress = getFirstMacAddress();
+export let machineId = macAddress ? parseInt(macAddress.replace(/\:|\D+/gi, '')).toString(36) : '';
